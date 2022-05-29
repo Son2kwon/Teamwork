5f96d746abf21ce3558eb025924f9c3537807337
taejigun <taejigun@naver.com>
2022-05-29 22:48:59 +0900

    수정합니다

--- 2week.cpp
+++ 2week.cpp
@@ -1,147 +1,213 @@
-﻿// 2week.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <bangtal.h>
+﻿#include <bangtal>
+using namespace bangtal;
 #include <stdio.h>
 #include <stdlib.h>
-SceneID scene1;
+#include <stdbool.h>
+
+
+#define ANIMATION_TIME		0.1f	//움직이는 시간 정의
+#define ANIMATION_STEP		150	//점프할때 이동하는 발걸음 y좌표
+#define DELAY_TIME 0.7f	//점프키:릴리즈 처리 시간 정의, 체공 시간을 장애물에 맞춰서 조정함
+
+SceneID start_scene, main_scene, end_scene;
+ObjectID playscene; //배경화면 오브젝트
+ObjectID cat, silver, gold; //움직이는 오브젝트(고양이), 은색 코인, 금색 코인 선언
+int coin_silverX = 300, coin_silverY = 300; //은색 코인 선언
+int coin_goldX = 500, coin_goldY = 500; //금색 코인 선언
+int coin_count; //점수
+
+TimerID timerRelease = createTimer(DELAY_TIME);	//점프키 릴리즈 처리 타이머(점프 시간)
+TimerID sceneTimer = createTimer(10000); //게임 총 시간 세는 타이머
+
+int x = 200, y = 50;	//초기 물체 좌표
+int dx = 0, dy = 0;		//물체 이동 좌표 정의
+
+KeyState keyState = KeyState::KEY_RELEASED;	//키보드 상태 정의
+
+void keyboardCallback(KeyCode code, KeyState state);	//키보드 콜백 함수
+void timerCallback(TimerID timer);	//타이머 콜백 함수
+
+//장애물
 ObjectID ob[4];
-int x = 0, speed = 20;
-TimerID timer1, timer2, timer3;
+int ob_x[4]; //장애물의 x좌표
+int ob_dx = 0, speed = 7;
+int ob_check[4];//장애물의 이미지를 확인하기 위한 변수
 
-int ob_x[4];
-
-ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown) {
+TimerID ob_speed_timer; //게임이 진행될수록 장애물이 이동하는 속도가 빨라지게 조절하는 타이머
+TimerID ob_timer; //장애물 배치, 장애물이 이동하도록 하는 타이머
+TimerID check_timer; //장애물과 쿠키의 충돌 확인하는 타이머
+//오브젝트 선언하는 함수
+ObjectID createObject(const char* name, const char* image, SceneID scene, int x, int y, bool shown) {
 	ObjectID object = createObject(image);
 	locateObject(object, scene, x, y);
+	showObject(object);
 
 	if (shown) {
 		showObject(object);
 	}
-	return object;
 
+	return object;
 }
 
-void keyboardCallback(KeyCode code, KeyState state) {
-	if (code == KeyCode::KEY_SPACE) {
+//캐릭터가 코인에 부딪혔는지 체크하는 함수.
+//부딪히면 코인이 사라지면서 점수가 증가
+void CoinCheck() {
+	if (coin_silverX < 100 + 576 && coin_silverX > 100 && coin_silverY > 100 && coin_silverY < 100 + 324) {
+		hideObject(silver);
+		coin_count += 1;
+	}
 
+	if (coin_goldX < 100 + 576 && coin_goldX > 100 && coin_goldY > 100 && coin_goldY < 100 + 324) {
+		hideObject(gold);
+		coin_count += 10;
 	}
 }
-void tiemrCallback(TimerID timer) {
-	if (timer == timer3) {
+
+void keyboardCallback(KeyCode code, KeyState state)	//점프키 이중점프를 위해 점프키:릴리즈 상태->타이머에서 처리
+{
+	if (code == KeyCode::KEY_SPACE && state == KeyState::KEY_PRESSED) {
+		if (y > 50 + ANIMATION_STEP) return;	//2단 점프만 가능하게 만들기
+		y += ANIMATION_STEP;
+		locateObject(cat, main_scene, x, y);
+		stopTimer(timerRelease);	//이전에 실행된 타이머 취소
+		setTimer(timerRelease, DELAY_TIME);	//점프키 릴리즈 타이머 다시 실행
+		startTimer(timerRelease);	//타이머 시작
+	}
+}
+
+void timerCallback(TimerID timer)
+{
+	if (timer == timerRelease)
+	{
+		y = 50;
+		locateObject(cat, main_scene, x, y);	//움직이는 물체 다시 돌아오게 만들기
+	}
+
+	if (timer == ob_timer) {
 		for (int i = 0; i < 4; i++) {
 			hideObject(ob[i]);//필요없을지도
 			if (ob_x[i] >= -100) {
 				ob_x[i] -= speed;
-				locateObject(ob[i], scene1, ob_x[i], 0);
+				locateObject(ob[i], main_scene, ob_x[i], 50);
 				showObject(ob[i]);//필요없을지도
 			}
+
 			else if (ob_x[i] < -100) {
-				startTimer(timer1);
-				
-				if (i == 0) {					
+				//startTimer(ob_speed_timer);  처음에는 괜찬은 거 같았는데 이렇게 하면 안될 것 같아서 main함수로 옮겼어요
+
+				if (i == 0) {
 					ob_x[i] = 1800;
-					int n = rand() % 3;
+					int n = rand() % 2;
 					switch (n) {
-					case 0: setObjectImage(ob[i], "gift.jpg"); break;
-					case 1: setObjectImage(ob[i], "end.png"); break;
-					case 2: setObjectImage(ob[i], "tree.png"); break;
+					case 0: setObjectImage(ob[i], "images\\ob_low.png"); ob_check[i] = 1; break;
+					case 1: setObjectImage(ob[i], "images\\ob_high.png"); ob_check[i] = 2; break;
 					}
 					break;
-					locateObject(ob[i], scene1, ob_x[i], 0);
+					locateObject(ob[i], main_scene, ob_x[i], 50);
 					showObject(ob[i]);
-					
+
 				}
 
 
 				else if (i == 1) {
-					int n = rand() % 3;
+					int n = rand() % 2;
 					switch (n) {
-					case 0: ob_x[i] = ob_x[0] + 400 + x, setObjectImage(ob[i], "gift.jpg"); break;
-					case 1: ob_x[i] = ob_x[0] + 450 + x, setObjectImage(ob[i], "end.png"); break;
-					case 2: ob_x[i] = ob_x[0] + 500 + x, setObjectImage(ob[i], "tree.png"); break;
-					}				
-					locateObject(ob[i], scene1, ob_x[i], 0);
+					case 0: ob_x[i] = ob_x[0] + 400 + ob_dx, setObjectImage(ob[i], "images\\ob_low.png"); ob_check[i] = 1; break;
+					case 1: ob_x[i] = ob_x[0] + 500 + ob_dx, setObjectImage(ob[i], "images\\ob_high.png"); ob_check[i] = 2; break;
+					}
+					locateObject(ob[i], main_scene, ob_x[i], 50);
 					showObject(ob[i]);
 				}
-				
+
 				else if (i == 2) {
-					int n = rand() % 3;
+					int n = rand() % 2;
 					switch (n) {
-					case 0: ob_x[i] = ob_x[1] + 400 + x, setObjectImage(ob[i], "gift.jpg"); break;
-					case 1: ob_x[i] = ob_x[1] + 450 + x, setObjectImage(ob[i], "tree.png"); break;
-					case 2: ob_x[i] = ob_x[1] + 500 + x, setObjectImage(ob[i], "end.png"); break;
+					case 0: ob_x[i] = ob_x[1] + 400 + ob_dx, setObjectImage(ob[i], "images\\ob_low.png"); ob_check[i] = 1; break;
+					case 1: ob_x[i] = ob_x[1] + 500 + ob_dx, setObjectImage(ob[i], "images\\ob_high.png"); ob_check[i] = 2; break;
 					}
-					locateObject(ob[i], scene1, ob_x[i], 0);
+					locateObject(ob[i], main_scene, ob_x[i], 50);
 					showObject(ob[i]);
-			
+
 				}
 				else if (i == 3) {
-					int n = rand() % 3;
+					int n = rand() % 2;
 					switch (n) {
-					case 0: ob_x[i] = ob_x[2] + 400 + x, setObjectImage(ob[i], "gift.jpg"); break;
-					case 1: ob_x[i] = ob_x[2] + 450 + x, setObjectImage(ob[i], "tree.png"); break;
-					case 2: ob_x[i] = ob_x[2] + 500 + x, setObjectImage(ob[i], "end.png"); break;
+					case 0: ob_x[i] = ob_x[2] + 400 + ob_dx, setObjectImage(ob[i], "images\\ob_low.png"); ob_check[i] = 1; break;
+					case 1: ob_x[i] = ob_x[2] + 500 + ob_dx, setObjectImage(ob[i], "images\\ob_high.png"); ob_check[i] = 2; break;
 					}
-					locateObject(ob[i], scene1, ob_x[i], 0);
+					locateObject(ob[i], main_scene, ob_x[i], 50);
 					showObject(ob[i]);
 
 				}
 			}
-			
-		}
-		setTimer(timer3, 0.1f);
-		startTimer(timer3);
-	}
-	if (timer == timer1) {
-		if (speed <= 50) {
-			x += 2;
-			speed += 2;
-			setTimer(timer1, 0.1f);
 
 		}
+		setTimer(ob_timer, 0.01f);
+		startTimer(ob_timer);
 	}
-	
+	if (timer == ob_speed_timer) {
+		ob_dx += 1;
+		speed += 1;
+		setTimer(ob_speed_timer, 5.0f);
+
+
+	}
+	if (timer == check_timer) {   //0.01초마다 충돌 확인, check number를 이용해서 장애물의 이미지를 구분, 쿠키 점프할 때 중간 위치변경이 없어서 y = 50, 200을 기준으로 충돌 확인함.
+
+		for (int i = 0; i < 4; i++) {
+			if (ob_x[i] < 271 && ob_x[i]> 74) {
+				if (ob_check[i] == 1) {  // 낮은 장애물의 경우
+					if (y == 50) {
+						endGame();
+					}
+				}
+				else if (ob_check[i] == 2) {
+					if (y == 50 || y == 200) {  //높은 장애물의 경우
+						endGame();
+					}
+				}
+			}
+		}
+		setTimer(check_timer, 0.01f);
+		startTimer(check_timer);
+	}
 }
 
-
-
-
-int main()
-{
-	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
+int main() {
+	//setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
 	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
 	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
-	
-	setKeyboardCallback(keyboardCallback);
-	setTimerCallback(tiemrCallback);
-	scene1 = createScene("back");
-	
-	timer1 = createTimer(0.1f);
-	
-	timer3 = createTimer(0.01f);
 
-	
+	start_scene = createScene("시작 화면", "");
+	main_scene = createScene("게임 화면", "images\\main_scene.png");
+	end_scene = createScene("종료 화면", "");
+
+	setTimerCallback(timerCallback);
+	setKeyboardCallback(keyboardCallback);
+
+	playscene = createObject("playscene.png");	//배경화면
+	locateObject(playscene, main_scene, 0, 0);
+	showObject(playscene);
+
+	cat = createObject("cat", "images\\cat.png", main_scene, x, y, true);
+	silver = createObject("silver", "images\\silver.png", main_scene, coin_silverX, coin_silverY, true);
+	gold = createObject("gold", "images\\gold.png", main_scene, coin_goldX, coin_goldY, true);
+
+	CoinCheck();
+
+	ob_speed_timer = createTimer(5.0f);
+	ob_timer = createTimer(0.01f);
+	check_timer = createTimer(0.01f);
 
 	for (int i = 0; i < 4; i++) {
 		ob_x[i] = 500 + 500 * i;
-;		ob[i] = createObject("gift.jpg", scene1, ob_x[i], 0, true);
+		ob_check[i] = 1;
+		ob[i] = createObject("ob_block", "images\\ob_low.png", main_scene, ob_x[i], 50, true);
 	}
-	startTimer(timer3);
-	
-	startGame(scene1);
-	
+
+	startTimer(ob_timer);
+	startTimer(check_timer);
+	startTimer(ob_speed_timer);
+	//startGame(start_scene);
+	startGame(main_scene);
 }
-
-
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
